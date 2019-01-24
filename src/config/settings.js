c320f65ba7ccf4ff6e1a3c00a5fe341e3c47c6c3
Ivan Badichan <badican01117@gmail.com>
2019-01-24 13:12:34 +0100

    Created product photo gallery

---
+++ src/config/settings.js
@@ -0,0 +1,16 @@
+const productGallery = {
+  slideWidth: 105,
+  slideCount: 2
+};
+
+const contentful = {
+  baseUrl: 'https://cdn.contentful.com',
+  spaceId: 'ywgv1cz3i98r',
+  accessToken: 'f9c5ef254b573c9b9815bdc186df08d8a8fc9794344913db35d4babcd222f949',
+  environment: 'master'
+};
+
+export {
+  contentful,
+  productGallery
+};
--- src/pages/product/Photos.js
+++ src/pages/product/Photos.js
@@ -1,18 +1,93 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Image from '../../common/components/product/Image';
+import Gallery from './gallery/Gallery';
 import PropTypes from 'prop-types';
+import { productGallery } from './../../config/settings';
 
-function Photos({ product }) {
-  return (
-    <section className='product-photos'>
-      <h2>Product photos:</h2>
+const slideWidth = productGallery.slideWidth;
+const slideCount = productGallery.slideCount;
 
-      <Image
-        {...product.photoFull}
-        className='product-photo-full'
-      />
-    </section>
-  );
+class Photos extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photoFull: props.product.photoFull,
+      galleryPosition: 0
+    };
+
+    this.handleThumbClick = this.handleThumbClick.bind(this);
+    this.moveGalleryBackward = this.moveGalleryBackward.bind(this);
+    this.moveGalleryForward = this.moveGalleryForward.bind(this);
+  }
+
+  componentDidMount() {
+    const photos = document.querySelectorAll('.product-gallery-list img');
+    let img;
+
+    photos.forEach(photo => {
+      img = document.createElement('img');
+      img.src = photo.parentNode.href;
+    });
+  }
+
+  handleThumbClick(event) {
+    event.preventDefault();
+    const currentTarget = event.currentTarget;
+    let target = event.target;
+
+    while (target != currentTarget) {
+      if (target.nodeName == 'A') {
+        const photoFull = Object.assign(
+          {},
+          this.state.photoFull,
+          { src: target.href, alt: target.title }
+        );
+        this.setState({ photoFull });
+        break;
+      }
+
+      target = target.parentNode;
+    }
+  }
+
+  moveGalleryBackward() {
+    const galleryPosition = Math.min(
+      this.state.galleryPosition + slideWidth * slideCount, 0
+    );
+
+    this.setState({ galleryPosition });
+  }
+
+  moveGalleryForward() {
+    const slides = document.querySelectorAll('.product-gallery-list li');
+    const galleryPosition = Math.max(
+      this.state.galleryPosition - slideWidth * slideCount,
+      -slideWidth * (slides.length - slideCount)
+    );
+
+    this.setState({ galleryPosition });
+  }
+
+  render() {
+    const product = this.props.product;
+    const photoFull = this.state.photoFull;
+    const galleryPosition = this.state.galleryPosition;
+
+    return (
+      <section className='product-photos'>
+        <h2 className='visually-hidden'>Product photos</h2>
+
+        <Image {...photoFull} className='product-photo-full' />
+        <Gallery
+          product={product}
+          onThumbClick={this.handleThumbClick}
+          moveBackward={this.moveGalleryBackward}
+          moveForward={this.moveGalleryForward}
+          position={galleryPosition}
+        />
+      </section>
+    );
+  }
 }
 
 Photos.propTypes = {
