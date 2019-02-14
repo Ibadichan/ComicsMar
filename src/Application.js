b3484ce8a1cd880163efd177c49ff62558920fe2
Ivan Badichan <badican01117@gmail.com>
2019-02-14 09:17:01 +0100

    Added redux to project, fetching products

--- src/Application.js
+++ src/Application.js
@@ -6,69 +6,21 @@ import routes from './routes/index';
 import RouteWithSubRoutes from './common/components/RouteWithSubRoutes';
 import ScrollToTop from './common/components/ScrollToTop';
 
-import request from 'superagent';
-import settings from './config/settings';
-const { baseUrl, spaceId, accessToken, environment  } = settings.contentful;
+import { connect } from 'react-redux';
+import { fetchProducts } from './actions/products';
 
 import Header from './common/components/header/Header';
 import Footer from './common/components/Footer';
 import Spinner from './common/components/Spinner';
 
-import AddPurchaseContext from './contexts/cart/AddPurchaseContext';
-import CartAmountContext from './contexts/cart/CartAmountContext';
-import ProductListContext from './contexts/products/ProductListContext';
-
 class Application extends Component {
   constructor(props) {
     super(props);
-    this.state = { purchases: [], products: [], loading: true };
     this.addPurchase = this.addPurchase.bind(this);
   }
 
   componentDidMount() {
-    request
-      .get(`${baseUrl}/spaces/${spaceId}/environments/${environment}/entries`)
-      .query({ content_type: 'product' })
-      .set('Authorization', `Bearer ${accessToken}`)
-      .then(response => this.parseProducts(response.body))
-      .then(products => this.setState(() => ({ products, loading: false })))
-      .catch(error => console.error(error));
-  }
-
-  parseProducts({ items, includes }) {
-    const assets = includes.Asset;
-    const products = items.map(({ fields }) => {
-      fields.photoFull   = this.findProductPhotos(assets, fields.photoFull);
-      fields.largePhotos = this.findProductPhotos(assets, fields.largePhotos);
-      fields.smallPhotos = this.findProductPhotos(assets, fields.smallPhotos);
-
-      return fields;
-    });
-
-    return products;
-  }
-
-  findProductPhotos(assets, photos) {
-    photos = Array.isArray(photos) ? photos : [photos];
-
-    const parsedPhotos = photos.map(photo => {
-      let asset, assetFields;
-
-      for (let i = 0; i < assets.length; i++) {
-        asset = assets[i];
-        assetFields = asset.fields;
-
-        if (asset.sys.id !== photo.sys.id) { continue };
-        return {
-          src: assetFields.file.url,
-          alt: assetFields.title,
-          width: assetFields.file.details.image.width,
-          height: assetFields.file.details.image.height
-        };
-      };
-    });
-
-    return (parsedPhotos.length > 1 ? parsedPhotos : parsedPhotos[0]);
+    this.props.fetchProducts();
   }
 
   addPurchase(event, purchase, quantity) {
@@ -90,30 +42,44 @@ class Application extends Component {
   }
 
   render() {
-    if (this.state.loading) { return <Spinner /> };
+    if (this.props.isFetching) { return <Spinner /> };
 
     return (
-      <AddPurchaseContext.Provider value={this.addPurchase}>
-      <CartAmountContext.Provider value={this.state.purchases}>
-      <ProductListContext.Provider value={this.state.products}>
-        <Router history={history}>
-          <ScrollToTop>
-            <StrictMode>
-              <Header />
-              <Switch>
-                {
-                  routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)
-                }
-              </Switch>
-              <Footer />
-            </StrictMode>
-          </ScrollToTop>
-        </Router>
-      </ProductListContext.Provider>
-      </CartAmountContext.Provider>
-      </AddPurchaseContext.Provider>
+      <p>Hello Redux !!!</p>
+      // <Router history={history}>
+      //   <StrictMode>
+      //     <ScrollToTop />
+
+      //     <Header />
+      //     <Switch>
+      //       {
+      //         routes.map((route, i) => <RouteWithSubRoutes key={i} {...route} />)
+      //       }
+      //     </Switch>
+      //     <Footer />
+      //   </StrictMode>
+      // </Router>
     );
   }
 }
 
-export default Application;
+function mapStateToProps(state) {
+  return {
+    isFetching: state.products.isFetching
+  };
+}
+
+function mapActionsToProps(dispatch) {
+  return {
+    fetchProducts() {
+      dispatch(fetchProducts());
+    }
+  };
+}
+
+const connectedApplication = connect(
+  mapStateToProps,
+  mapActionsToProps
+)(Application);
+
+export default connectedApplication;
