3438e60d50f9d96659bb443d4ac1ede4324c79ff
Ivan Badichan <badican01117@gmail.com>
2019-05-01 19:41:13 +0200

    Added SCSS in project :fire:

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -3,31 +3,36 @@ import React from 'react';
 function Footer() {
   return (
     <footer className='main-footer'>
-      <div className='social-list-wrapper'>
-        <b>Keep connected:</b>
-        <ul className='social-list'>
-          <li>
-            <a href='#facebook' className='social-button-facebook'>
-              <span className='visually-hidden'>Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a href='#vkontakte' className='social-button-vkontakte'>
-              <span className='visually-hidden'>Vkontakte</span>
-            </a>
-          </li>
-          <li>
-            <a href='#twitter' className='social-button-twitter'>
-              <span className='visually-hidden'>Twitter</span>
-            </a>
-          </li>
-        </ul>
-      </div>
+      <div className='main-footer-container'>
+        <div className='social-list-wrapper'>
+          <b>Keep connected:</b>
+          <ul className='social-list'>
+            <li>
+              <a href='#facebook' className='social-button-facebook'>
+                <span className='visually-hidden'>Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href='#vkontakte' className='social-button-vkontakte'>
+                <span className='visually-hidden'>Vkontakte</span>
+              </a>
+            </li>
+            <li>
+              <a href='#twitter' className='social-button-twitter'>
+                <span className='visually-hidden'>Twitter</span>
+              </a>
+            </li>
+          </ul>
+        </div>
 
-      <p className='site-is-fake'>
-        This site is fake and does not sell anything, the real owner is here
-        <a href='https://www.marvel.com' target='_blank'>https://www.marvel.com</a>
-      </p>
+        <p className='site-is-fake'>
+          This site is fake and does not sell anything,
+          the real owner is here
+          <a href='https://www.marvel.com' target='_blank'>
+            https://www.marvel.com
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
