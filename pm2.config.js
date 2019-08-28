b17f8270dcf83cc8f57fa7d1bce955e935f8e718
Ivan Badichan <badican01117@gmail.com>
2019-08-28 14:27:24 +0200

    Created pm2.config.js config

---
+++ pm2.config.js
@@ -0,0 +1,27 @@
+const config = {
+  apps: [
+    {
+      name: "ComicsMar",
+      script: "./initializers/server/index.js",
+      cwd: "/home/deployer/ComicsMar/current",
+      instances: "max",
+      exec_mode: "cluster"
+    }
+  ],
+  deploy: {
+    production: {
+      user: "deployer",
+      host: ["79.143.28.56"],
+      ref: "origin/master",
+      repo: "git@github.com:Ibadichan/ComicsMar.git",
+      "post-deploy":
+        "npm install && " +
+        "npm run build && " +
+        "pm2 startOrRestart pm2.config.js --env production",
+      path: "/home/deployer/ComicsMar/current",
+      ssh_options: "StrictHostKeyChecking=no"
+    }
+  }
+};
+
+module.exports = config;
